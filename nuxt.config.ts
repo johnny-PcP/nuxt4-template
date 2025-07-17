fabadccb02377fbca32a7634a6cb63bb2e183b6d
Johnny-PcP <johnnypcp0313@gmail.com>
2025-07-17 13:01:09 +0800

    chore: 更新 .gitignore，調整 tsconfig.json 參考路徑，修正 nuxt.config.ts 相容性日期

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: '2025-05-15',
   modules: [
     '@nuxt/eslint',
   ],
